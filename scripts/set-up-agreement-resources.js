93adaa079de135fc442f45c9ad8b28319fcf0cdd
Taoufik Mohdit <91465696+taomoh@users.noreply.github.com>
2023-06-05 14:54:11 +0100

    Add logging flag (#1)
    * Add logging flag, set to false by default
    
    * Extract resource creation code into asynchronous functions
    Make maps agreement targets and accrual types into script global constants
    
    * Formatting
    
    ---------
    
    Co-authored-by: Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>

--- scripts/set-up-agreement-resources.js
+++ scripts/set-up-agreement-resources.js
@@ -3,13 +3,40 @@ const agreementId = jsonData.items[0].id
 postman.setEnvironmentVariable("agreementId", agreementId);
 const agreementStartDate =  jsonData.items[0].startDate;
 const agreementEndDate =  jsonData.items[0].endDate;
-// const agreementStartDate =  "2023-04-01";
-// const agreementEndDate =  "2023-04-05";
+const loggingEnabled = pm.environment.get('loggingEnabled') === 'true';
 
 const accrualsUrl = pm.environment.replaceIn(
     "{{accruals_service_url}}/resources/accruals?tenantId={{tenantId}}");
 const agreementTargetsUrl = pm.environment.replaceIn(
     "{{accruals_service_url}}/resources/agreement-targets?tenantId={{tenantId}}");
+
+const agreementTargets = {
+  "e502eebb-4663-4e5b-9445-9a20441c18d9": 2192,// Annual Target Hours
+  "5f06e6ce-1422-4a0c-89dd-f4952e735202": 104// Night Hours
+};
+// "05bbd915-e907-4259-a2e2-080d7956afec": 286,// Weekend Hours
+// "b94bb25a-7fe2-4599-91ab-f0d58e013aed": 32,// Public Holidays
+// "2a5ea69d-1a2c-409d-b430-43a5dbc403b3": 50,// On-call Weekday
+// "df4c4b08-ac4a-45e0-83bb-856d3219a8b3": 35,// On-call Weekend
+// "a628bf34-d834-437d-a57a-ed549bd9a330": 2,// On-call Public Holiday
+// "c4fd5435-8239-4f1f-9c4b-7f458b7b636d": 0,// Flexible Credits
+// "c73030ed-ed28-4d59-85e8-185f70d85a94": 0,// Rostered Shift Allowance
+// "787d2d12-2aff-4253-b382-bcefded61124": 64,// Public Holiday Credit
+// };
+
+const accrualTypes = {
+  "e502eebb-4663-4e5b-9445-9a20441c18d9": "Annual Target Hours",
+  "5f06e6ce-1422-4a0c-89dd-f4952e735202": "Night Hours"
+};
+//     "05bbd915-e907-4259-a2e2-080d7956afec": "Weekend Hours",
+//     "b94bb25a-7fe2-4599-91ab-f0d58e013aed": "Public Holidays",
+//     "2a5ea69d-1a2c-409d-b430-43a5dbc403b3": "On-call Weekday",
+//     "df4c4b08-ac4a-45e0-83bb-856d3219a8b3": "On-call Weekend",
+//     "a628bf34-d834-437d-a57a-ed549bd9a330": "On-call Public Holiday",
+//     "c4fd5435-8239-4f1f-9c4b-7f458b7b636d": "Flexible Credits",
+//     "c73030ed-ed28-4d59-85e8-185f70d85a94": "Rostered Shift Allowance",
+//     "787d2d12-2aff-4253-b382-bcefded61124": "Public Holiday Credit"
+// };
 
 pm.sendRequest("https://cdnjs.cloudflare.com/ajax/libs/js-joda/1.11.0/js-joda.min.js", (err, res) => {
   //convert the response to text and save it as an environment variable
@@ -47,6 +74,47 @@ pm.sendRequest("https://cdnjs.cloudflare.com/ajax/libs/js-joda/1.11.0/js-joda.mi
     };
   }
 
+  function sendRequest(req) {
+    return new Promise((resolve, reject) => {
+      pm.sendRequest(req, (err, res) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(res);
+      })
+    });
+  }
+
+  const creatAccrualPlaceholders = async function(accrualDates, accrualTypes) {
+    for (const accrualDate of accrualDates) {
+      pm.collectionVariables.set("accrualDate", accrualDate);
+      for (const [key, value] of Object.entries(accrualTypes)) {
+
+        // accrualTypes map is key'd by accrualTypeId, so key == accrualTypeId
+        let request = buildRequest('POST', accrualsUrl, buildAccrual(accrualDate, key, 0, 0));
+
+        let result = await sendRequest(request);
+        if (result.code === 200 && loggingEnabled) {
+          console.log(
+              `"${value}" Accrual placeholder created successfully for ${accrualDate}`);
+        }
+      }
+    }
+  }
+
+  const createAgreementTargets = async function(agreementTargets, accrualTypes) {
+    for (const [key, value] of Object.entries(agreementTargets)) {
+      let accrualType = accrualTypes[key];
+
+      // agreementTargets map is key'd by accrualTypeId, so key == accrualTypeId and value = totalTarget
+      let request = buildRequest('POST', agreementTargetsUrl, buildAgreementTarget(key, value));
+      let result = await sendRequest(request)
+      if (result.code === 200 && loggingEnabled) {
+        console.log(`"${accrualType}" Agreement Target created successfully`);
+      }
+    }
+  }
+
   const buildAgreementTarget = function(accrualTypeId, targetTotal) {
     return {
       "tenantId": pm.environment.get('tenantId'),
@@ -55,34 +123,6 @@ pm.sendRequest("https://cdnjs.cloudflare.com/ajax/libs/js-joda/1.11.0/js-joda.mi
       "targetTotal": targetTotal
     };
   }
-
-  const agreementTargets = {
-    "e502eebb-4663-4e5b-9445-9a20441c18d9": 2192,// Annual Target Hours
-    "5f06e6ce-1422-4a0c-89dd-f4952e735202": 104// Night Hours
-  };
-  // "05bbd915-e907-4259-a2e2-080d7956afec": 286,// Weekend Hours
-  // "b94bb25a-7fe2-4599-91ab-f0d58e013aed": 32,// Public Holidays
-  // "2a5ea69d-1a2c-409d-b430-43a5dbc403b3": 50,// On-call Weekday
-  // "df4c4b08-ac4a-45e0-83bb-856d3219a8b3": 35,// On-call Weekend
-  // "a628bf34-d834-437d-a57a-ed549bd9a330": 2,// On-call Public Holiday
-  // "c4fd5435-8239-4f1f-9c4b-7f458b7b636d": 0,// Flexible Credits
-  // "c73030ed-ed28-4d59-85e8-185f70d85a94": 0,// Rostered Shift Allowance
-  // "787d2d12-2aff-4253-b382-bcefded61124": 64,// Public Holiday Credit
-  // };
-
-  const accrualTypes = {
-    "e502eebb-4663-4e5b-9445-9a20441c18d9": "Annual Target Hours",
-    "5f06e6ce-1422-4a0c-89dd-f4952e735202": "Night Hours"
-  };
-  //     "05bbd915-e907-4259-a2e2-080d7956afec": "Weekend Hours",
-  //     "b94bb25a-7fe2-4599-91ab-f0d58e013aed": "Public Holidays",
-  //     "2a5ea69d-1a2c-409d-b430-43a5dbc403b3": "On-call Weekday",
-  //     "df4c4b08-ac4a-45e0-83bb-856d3219a8b3": "On-call Weekend",
-  //     "a628bf34-d834-437d-a57a-ed549bd9a330": "On-call Public Holiday",
-  //     "c4fd5435-8239-4f1f-9c4b-7f458b7b636d": "Flexible Credits",
-  //     "c73030ed-ed28-4d59-85e8-185f70d85a94": "Rostered Shift Allowance",
-  //     "787d2d12-2aff-4253-b382-bcefded61124": "Public Holiday Credit"
-  // };
 
   const buildRequest = function(method, url, body) {
     return {
@@ -99,36 +139,13 @@ pm.sendRequest("https://cdnjs.cloudflare.com/ajax/libs/js-joda/1.11.0/js-joda.mi
   }
 
   const accrualDates = datesBetween(agreementStartDate,agreementEndDate);
-  console.log("Accrual Dates: ", accrualDates);
 
-  // create accrual placeholders
-  for (const accrualDate of accrualDates) {
-    pm.collectionVariables.set("accrualDate", accrualDate);
-    for (const [key, value] of Object.entries(accrualTypes)) {
-
-      // accrualTypes map is key'd by accrualTypeId, so key == accrualTypeId
-      let request = buildRequest('POST', accrualsUrl, buildAccrual(accrualDate, key, 0, 0));
-
-      pm.sendRequest(request, function (error, result) {
-            if (result.code === 200) {
-              console.log(`"${value}" Accrual placeholder created successfully for ${accrualDate}`);
-            }
-
-      });
-    }
+  const createAll = async function (accrualDates, accrualTypes, agreementTargets) {
+    await creatAccrualPlaceholders(accrualDates, accrualTypes);
+    await createAgreementTargets(agreementTargets, accrualTypes)
   }
 
-  // create agreement targets
-  for (const [key, value] of Object.entries(agreementTargets)) {
-    let accrualType = accrualTypes[key];
-
-    // agreementTargets map is key'd by accrualTypeId, so key == accrualTypeId and value = totalTarget
-    let request = buildRequest('POST', agreementTargetsUrl, buildAgreementTarget(key, value));
-    pm.sendRequest(request, function (error, result) {
-      if (result.code === 200) {
-        console.log(`"${accrualType}" Agreement Target created successfully`);
-      }
-    });
-  }
+  createAll(accrualDates, accrualTypes, agreementTargets)
+  .then(() => console.log('Agreement data setup complete!'));
 
 });
